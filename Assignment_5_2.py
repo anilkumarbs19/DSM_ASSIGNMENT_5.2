bbdff44303ac0c298e86013ebb41bf891e43721e
anilkumarbs19 <anilkumarbs19@gmail.com>
2018-04-03 20:44:26 +0530

    First commit

---
+++ Assignment_5_2.py
@@ -0,0 +1,10 @@
+subject=["Americans","Indians"]
+verbs=["play","watch"]
+objects=["Baseball","Cricket"]
+
+
+for i in subject:
+    for j in verbs:
+        for k in objects:
+# print the desired output
+            print("\n"+i+" "+j+" "+k+".")
